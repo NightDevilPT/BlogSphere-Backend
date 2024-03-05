01b26d3ade3c044679a015d33de5dfdfd5168e28
NightDevilPT <pawankumartadagsingh@gmail.com>
2024-03-05 07:35:06 +0530

    modifing-folder-file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,57 +1,13 @@
-import { AppService } from './app.service';
 import { AppController } from './app.controller';
 
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-
-import { BlogModule } from './blog/blog.module';
-import { UserModule } from './user/user.module';
-import { BlogEntity } from './blog/entities/blog.entity';
-import { ProfileModule } from './profile/profile.module';
-import { UserEntity } from './user/entities/user.entity';
-import { CommentModule } from './comment/comment.module';
-import { EmailService } from './service/email-service.service';
-import { ProfileEntity } from './profile/entities/profile.entity';
-import { CommentEntity } from './comment/entities/comment.entity';
-import { PasswordService } from './service/password-service.service';
-import { GlobalInterceptor } from './interceptors/guard.interceptor';
-import { JwtModule } from '@nestjs/jwt';
-import { JwtAuthService } from './service/jwt.service';
-import { PaginationService } from './service/pagination.service';
-import { AllEntities } from './module-index';
+import { AllModules, AllProviders } from './module-index';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: parseInt(process.env.POSTGRES_PORT),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DATABASE,
-      synchronize: true,
-      ssl: { rejectUnauthorized: false },
-      entities: [...AllEntities],
-    }),
-    UserModule,
-    ProfileModule,
-    BlogModule,
-    CommentModule,
-    JwtModule.register({
-      secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: process.env.JWT_EXPIREIN },
-    }),
+    ...AllModules
   ],
   controllers: [AppController],
-  providers: [
-    AppService,
-    EmailService,
-    PasswordService,
-    GlobalInterceptor,
-    JwtAuthService,
-    PaginationService,
-  ],
+  providers: [...AllProviders],
 })
 export class AppModule {}
--- src/module-index.ts
+++ src/module-index.ts
@@ -1,6 +1,56 @@
+import { ConfigModule } from '@nestjs/config';
 import { BlogEntity } from './blog/entities/blog.entity';
 import { CommentEntity } from './comment/entities/comment.entity';
 import { ProfileEntity } from './profile/entities/profile.entity';
 import { UserEntity } from './user/entities/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { ProfileModule } from './profile/profile.module';
+import { BlogModule } from './blog/blog.module';
+import { CommentModule } from './comment/comment.module';
+import { JwtModule } from '@nestjs/jwt';
+import { PaginationService } from './service/pagination.service';
+import { JwtAuthService } from './service/jwt.service';
+import { GlobalInterceptor } from './interceptors/guard.interceptor';
+import { PasswordService } from './service/password-service.service';
+import { EmailService } from './service/email-service.service';
+import { AppService } from './app.service';
 
-export const AllEntities = [UserEntity, ProfileEntity, BlogEntity, CommentEntity];
+export const AllEntities = [
+  UserEntity,
+  ProfileEntity,
+  BlogEntity,
+  CommentEntity,
+];
+
+export const AllModules = [
+  ConfigModule.forRoot(),
+  TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: process.env.POSTGRES_HOST,
+    port: parseInt(process.env.POSTGRES_PORT),
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    synchronize: true,
+    ssl: { rejectUnauthorized: false },
+    entities: [...AllEntities],
+  }),
+  UserModule,
+  ProfileModule,
+  BlogModule,
+  CommentModule,
+  JwtModule.register({
+    secret: process.env.JWT_SECRET,
+    signOptions: { expiresIn: process.env.JWT_EXPIREIN },
+  }),
+];
+
+export const AllProviders = [
+  AppService,
+  EmailService,
+  PasswordService,
+  GlobalInterceptor,
+  JwtAuthService,
+  PaginationService,
+];
